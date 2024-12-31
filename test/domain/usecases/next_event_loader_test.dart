323c7a226749fb71344e079b593e470264f727bd
hmartiins <hmartins224@gmail.com>
2024-12-31 16:15:09 -0300

    test: ensure NextEventLoader returns NextEvent on success

--- test/domain/usecases/next_event_loader_test.dart
+++ test/domain/usecases/next_event_loader_test.dart
@@ -1,29 +1,45 @@
 import 'dart:math';
 
+import 'package:advanced_flutter/domain/entities/next_event_player.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+class NextEvent {
+  final String groupName;
+  final DateTime date;
+  final List<NextEventPlayer> players;
+
+  NextEvent({
+    required this.groupName,
+    required this.date,
+    required this.players,
+  });
+}
 
 class NextEventLoader {
   final LoadNextEventRepository repo;
 
   NextEventLoader({required this.repo});
 
-  Future<void> call({required String groupId}) async {
-    await repo.loadNextEvent(groupId: groupId);
+  Future<NextEvent> call({required String groupId}) async {
+    return repo.loadNextEvent(groupId: groupId);
   }
 }
 
 abstract class LoadNextEventRepository {
-  Future<void> loadNextEvent({required String groupId});
+  Future<NextEvent> loadNextEvent({required String groupId});
 }
 
 class LoadNextEventMockRepository implements LoadNextEventRepository{
   String? groupId;
   var callsCount = 0;
+  NextEvent? output;
 
   @override
-  Future<void> loadNextEvent({required String groupId}) async {
+  Future<NextEvent> loadNextEvent({required String groupId}) async {
     this.groupId = groupId;
     callsCount++;
+
+    return output!;
   }
 }
 
@@ -35,6 +51,27 @@ void main() {
   setUp(() {
     groupdId = Random().nextInt(50_000).toString();
     repo = LoadNextEventMockRepository();
+    repo.output = NextEvent(
+      groupName: 'any_name',
+      date: DateTime.now(),
+      players: [
+        NextEventPlayer(
+          id: Random().nextInt(50_000).toString(),
+          name: 'Henrique Martins',
+          confirmationDate: DateTime.now(),
+          isConfirmed: true,
+          photo: 'any_photo',
+        ),
+        NextEventPlayer(
+          id: Random().nextInt(50_000).toString(),
+          name: 'Isaac Melo Alves Martins',
+          confirmationDate: DateTime.now(),
+          isConfirmed: false,
+          position: 'any_position',
+        ),
+      ],
+    );
+
     sut = NextEventLoader(repo: repo);
   });
 
@@ -43,5 +80,26 @@ void main() {
 
     expect(repo.groupId, groupdId);
     expect(repo.callsCount, 1);
+  });
+
+  test('should return event data on success ', () async {
+    final event = await sut(groupId: groupdId);
+
+    expect(event.groupName, repo.output?.groupName);
+    expect(event.date, repo.output?.date);
+
+    expect(event.players.length, 2);
+
+    expect(event.players[0].id, repo.output?.players[0].id);
+    expect(event.players[0].name, repo.output?.players[0].name);
+    expect(event.players[0].confirmationDate, repo.output?.players[0].confirmationDate);
+    expect(event.players[0].isConfirmed, repo.output?.players[0].isConfirmed);
+    expect(event.players[0].photo, repo.output?.players[0].photo);
+
+    expect(event.players[1].id, repo.output?.players[1].id);
+    expect(event.players[1].name, repo.output?.players[1].name);
+    expect(event.players[1].confirmationDate, repo.output?.players[1].confirmationDate);
+    expect(event.players[1].isConfirmed, repo.output?.players[1].isConfirmed);
+    expect(event.players[1].position, repo.output?.players[1].position);
   });
 }
