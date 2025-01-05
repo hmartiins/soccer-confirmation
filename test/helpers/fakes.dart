fb49f301b59a657c2e11d2738b36d7ad1411eafd
hmartiins <hmartins224@gmail.com>
2025-01-05 17:36:18 -0300

    test: ensure NextEventRxPresenter map NextEventPlayer to doubt player

--- test/helpers/fakes.dart
+++ test/helpers/fakes.dart
@@ -1,3 +1,5 @@
 import 'dart:math';
 
 String anyString() => Random().nextInt(1_000_000).toString();
+bool anyBool() => Random().nextBool();
+DateTime anyDateTime() => DateTime.fromMillisecondsSinceEpoch(Random().nextInt(1_000_000));
