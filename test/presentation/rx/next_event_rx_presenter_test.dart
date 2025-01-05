fd258efd9ee0f32bc8a37e00ea8c238fdc2bebfe
hmartiins <hmartins224@gmail.com>
2025-01-05 17:14:10 -0300

    test: ensure NextEventRxPresenter emits ViewModel on load and reload with success

--- test/presentation/rx/next_event_rx_presenter_test.dart
+++ test/presentation/rx/next_event_rx_presenter_test.dart
@@ -1,6 +1,7 @@
 @Timeout(Duration(seconds: 1))
 library;
 
+import 'package:advanced_flutter/presentation/presenters/next_event_presenter.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:rxdart/subjects.dart';
 
@@ -8,12 +9,12 @@ import '../../helpers/fakes.dart';
 
 final class NextEventRxPresenter {
   final Future<void> Function({required String groupId}) nextEventLoader;
-  final nextEventSubject = BehaviorSubject();
+  final nextEventSubject = BehaviorSubject<NextEventViewModel>();
   final isBusySubject = BehaviorSubject<bool>();
 
   NextEventRxPresenter({required this.nextEventLoader});
 
-  Stream get nextEventStream => nextEventSubject.stream;
+  Stream<NextEventViewModel> get nextEventStream => nextEventSubject.stream;
   Stream<bool> get isBusyStream => isBusySubject.stream;
 
   Future<void> loadNextEvent({
@@ -23,6 +24,7 @@ final class NextEventRxPresenter {
     try {
       if (isReload) isBusySubject.add(true);
       await nextEventLoader(groupId: groupId);
+      nextEventSubject.add(const NextEventViewModel());
     } catch (error) {
       nextEventSubject.addError(error);
     } finally {
@@ -76,11 +78,19 @@ void main() {
 
   test('should emit correct events on reload with success', () async {
     expectLater(sut.isBusyStream, emitsInOrder([true, false]));
+    expectLater(
+      sut.nextEventStream,
+      emits(const TypeMatcher<NextEventViewModel>()),
+    );
     await sut.loadNextEvent(groupId: groupId, isReload: true);
   });
 
   test('should emit correct events on load with success', () async {
     sut.isBusyStream.listen(neverCalled);
+    expectLater(
+      sut.nextEventStream,
+      emits(const TypeMatcher<NextEventViewModel>()),
+    );
     await sut.loadNextEvent(groupId: groupId);
   });
 }
