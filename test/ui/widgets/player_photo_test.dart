e6b50c83cb1e40ae2573a1768a7466153cf7c28f
hmartiins <hmartins224@gmail.com>
2025-01-05 01:31:17 -0300

    test: ensure PlayerPhoto hide initials when there is photo

--- test/ui/widgets/player_photo_test.dart
+++ test/ui/widgets/player_photo_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:network_image_mock/network_image_mock.dart';
 
 class PlayerPhoto extends StatelessWidget {
   final String initials;
@@ -14,7 +15,8 @@ class PlayerPhoto extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CircleAvatar(
-      child: Text(initials),
+      foregroundImage: photo != null ? NetworkImage(photo!) : null,
+      child: photo == null ? Text(initials) : null,
     );
   }
 }
@@ -27,5 +29,16 @@ void main() {
       ),
     );
     expect(find.text('HM'), findsOneWidget);
+  });
+
+  testWidgets('should hide initals when there is photo', (tester) async {
+    mockNetworkImagesFor(() async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: PlayerPhoto(initials: 'HM', photo: 'http://photo.com'),
+        ),
+      );
+      expect(find.text('HM'), findsNothing);
+    });
   });
 }
