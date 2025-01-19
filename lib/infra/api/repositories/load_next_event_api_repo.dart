57fa963d30d11b90fdffeb7f44a8ec2aeaba209d
hmartiins <hmartins224@gmail.com>
2025-01-18 21:41:28 -0300

    feat: run app with factories in main layer

--- lib/infra/api/repositories/load_next_event_api_repo.dart
+++ lib/infra/api/repositories/load_next_event_api_repo.dart
@@ -1,17 +1,15 @@
 import 'package:advanced_flutter/domain/entities/errors.dart';
 import 'package:advanced_flutter/domain/entities/next_event.dart';
-import 'package:advanced_flutter/domain/repositories/load_next_event_repository.dart';
 import 'package:advanced_flutter/infra/api/clients/http_get_client.dart';
 import 'package:advanced_flutter/infra/api/mappers/next_event_mapper.dart';
 
-final class LoadNextEventApiRepository implements LoadNextEventRepository {
+final class LoadNextEventApiRepository {
   final HttpGetClient httpClient;
   final String url;
 
   const LoadNextEventApiRepository(
       {required this.httpClient, required this.url});
 
-  @override
   Future<NextEvent> loadNextEvent({required String groupId}) async {
     final json = await httpClient.get(url: url, params: {'groupId': groupId});
 
