ae3a44a8d5ac91954db61f42703b4c319acf23bf
hmartiins <hmartins224@gmail.com>
2025-01-21 19:58:35 -0300

    test: ensure CacheManagerAdapter return json if cache is valid

--- test/infra/cache/adapters/cache_manager_adapter_test.dart
+++ test/infra/cache/adapters/cache_manager_adapter_test.dart
@@ -2,8 +2,7 @@ import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:file/file.dart';
-import 'package:flutter_cache_manager/flutter_cache_manager.dart'
-    hide FileSystem;
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import '../../../mocks/fakes.dart';
@@ -15,9 +14,18 @@ final class CacheManagerAdapter {
 
   Future<dynamic> get({required String key}) async {
     final info = await client.getFileFromCache(key);
-    await info?.file.exists();
-    await info?.file.readAsString();
-    return null;
+
+    if (info?.validTill.isBefore(DateTime.now()) != false ||
+        !await info!.file.exists()) {
+      return null;
+    }
+
+    final data = await info.file.readAsString();
+    try {
+      return jsonDecode(data);
+    } catch (e) {
+      return null;
+    }
   }
 }
 
@@ -29,6 +37,7 @@ final class FileSpy implements File {
 
   void simulateFileEmpty() => _fileExists = false;
   void simulateInvalidResponse() => _response = 'invalid_json';
+  void simulateResponse(String response) => _response = response;
 
   @override
   Future<String> readAsString({Encoding encoding = utf8}) async {
@@ -389,5 +398,17 @@ void main() {
     client.file.simulateInvalidResponse();
     final json = await sut.get(key: key);
     expect(json, isNull);
+  });
+
+  test('should return json if cache is valid', () async {
+    client.file.simulateResponse('''
+      {
+        "key1": "value1",
+        "key2": "value2"
+      }
+    ''');
+    final json = await sut.get(key: key);
+    expect(json['key1'], 'value1');
+    expect(json['key2'], 'value2');
   });
 }
